bacb5727bcdf148e83b7f30e81f5049f1eb5ebc0
I14I3TS450BW8C <I14I3TS450BW8C@192.168.85.1>
2017-02-19 22:28:16 -0600

    Primera modificacion

---
+++ prueba_diseno/src/prueba_diseno/Prueba_diseno.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_diseno;
+
+/**
+ *
+ * @author I14I3TS450BW8C
+ */
+public class Prueba_diseno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
